19154d1b0b8edf6287313c885c8b7c97d1f56696
Isaac Ramirez <isaac.ramirez.soccer@gmail.com>
2019-02-22 21:25:25 -0700

    end product

--- veneer.py
+++ veneer.py
@@ -2,27 +2,25 @@
 #Credits to Code Academy
 
 class Art:
-    def __init__(self, artist, title, year, medium):
+    def __init__(self, artist, title, year, medium, owner):
         self.artist = artist
         self.title = title
         self.year = year
         self.medium = medium
+        self.owner = owner
 
     def __repr__(self):
-        return """{artist}. "{title}". {year}, {medium}.""".format(artist=self.artist,
-        title=self.title, year=self.year, medium=self.medium)
+        return '%s. %s. %s. %s. %s, %s' % (self.artist, self.title, 
+        self.year, self.medium, self.owner.name, self.owner.location)
 
 class Marketplace:
-    def __init__(self, listings):
-        #listings = []
-        self.listings = listings
+    def __init__(self):
+        self.listings = []
 
     def add_listing(self, new_listing):
-        self.new_listing = []
         self.listings.append(new_listing)
 
     def remove_listing(self, expired_listing):
-        self.expired_listing = []
         self.listings.remove(expired_listing)
         
     def show_listing(self):
@@ -45,13 +43,43 @@ class Client:
         return "{name}; {location}; {is_museum}.".format(name=self.name,
         location=self.location, is_museum=self.is_museum)
 
+    def sell_artwork(self, artwork, price):
+        #price is in USD Millions
+        if artwork.owner == self:
+            new_listing = Listing(artwork.title, price, self)
+            veneer.add_listing(new_listing)
+
+    def buy_artwork(self, artwork):
+        if artwork.owner != self:
+            art_listing = None
+            for listing in veneer.listings:
+                if listing.art == artwork:
+                    art_listing = listing
+                    break
+            if art_listing != None:
+                art_listing.art.owner = self
+                veneer.remove_listing(art_listing)
+            
+
+class Listing:
+    def __init__(self, art, price, seller):
+        self.art = art
+        self.price = price
+        self.seller = seller
+
+    def __repr__(self):
+        return '%s. %s' % (self.art, self.price)
+
+
 
 moma = Client("The MOMA", "New York", True)
 edytta = Client("Edytta Halpirt", "Private Collection", False)
-veneer = Marketplace([])
-girl_with_mandolin = Art("Picasso, Pablo", "Girl with a Mandolin (Fanny Tellier)", 1910, "oil on canvas")
+veneer = Marketplace()
+girl_with_mandolin = Art("Picasso, Pablo", "Girl with a Mandolin (Fanny Tellier)", 1910, "oil on canvas", edytta)
+edytta.sell_artwork(girl_with_mandolin, 6)
 
-#executes repr() method
+
+#executes repr() method and added owner argument
 #print(girl_with_mandolin)
 
 #executes none rn, since we have nothing, therefore good
@@ -59,3 +87,10 @@ girl_with_mandolin = Art("Picasso, Pablo", "Girl with a Mandolin (Fanny Tellier)
 
 #executes repr() method
 # print(moma, edytta)
+
+#returns none; fix
+#print(veneer.show_listing())
+
+#moma.buy_artwork(girl_with_mandolin)
+
+#print(girl_with_mandolin)
